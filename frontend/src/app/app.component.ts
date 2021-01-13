dbce0bde4c8754a52aada842b31a3c93fa0d38bd
InceptionCode <darrellwjr.com@gmail.com>
2021-01-13 06:23:15 -0500

    removed dotenv, api call now properly unsubscribes.

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,28 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Apollo, gql } from 'apollo-angular';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'JustDoIt';
+  _testSubscription: Subscription = new Subscription;
 
   constructor(private apollo: Apollo) {}
 
   ngOnInit() {
-    this.apollo
+    this._testSubscription = this.apollo
       .watchQuery({
         query: gql`
           {
-            user(userId: "K7NG_k72_095ER_-lwtFr") {
-              username
+            todos {
+              text
+              createdBy {
+                username
+              }
+            },
+            tags {
+              label
             }
           }
           `
     }).valueChanges.subscribe((result: any) => {
       console.log(result.data)
     })
+  }
+
+  ngOnDestroy() {
+    this._testSubscription.unsubscribe()
   }
 }
